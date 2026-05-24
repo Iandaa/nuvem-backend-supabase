0ef5051d20a34e348b37f3caef125503773fbd97
Iandeyara Farias <iandalorrainec@gmail.com>
2026-05-24 08:53:43 -0300

    fix api json responses

--- app.js
+++ app.js
@@ -15,66 +15,89 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const cors=require("cors");
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
+const cors = require("cors");
+
+const corsOptions = {
+   origin: '*',
+   credentials: true,            //access-control-allow-credentials:true
+   optionSuccessStatus: 200,
 }
 
 app.use(cors(corsOptions)) // Use this after the variable declaration
 
-
 // using morgan for logs
 app.use(morgan('combined'));
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const supabase = 
-    supabaseClient.createClient('https://zvdqclbyyjcayxmbrxab.supabase.co/rest/v1/', 
-        'sb_publishable_42j06zG40bipp1q3BicpQQ_AkG6KCwZ')
-
+const supabase =
+    supabaseClient.createClient(
+        'https://zvdqclbyyjcayxmbrxab.supabase.co',
+        'SUA_CHAVE_SUPABASE'
+    )
 
 app.get('/products', async (req, res) => {
-    const {data, error} = await supabase
+
+    const { data, error } = await supabase
         .from('products')
         .select()
-    res.send(data);
-    console.log(`lists all products${data}`);
+
+    if (error) {
+        console.log(error);
+        return res.status(500).json(error);
+    }
+
+    console.log(data);
+
+    res.json(data);
 });
 
 app.get('/products/:id', async (req, res) => {
+
     console.log("id = " + req.params.id);
-    const {data, error} = await supabase
+
+    const { data, error } = await supabase
         .from('products')
         .select()
         .eq('id', req.params.id)
-    res.send(data);
 
-    console.log("retorno "+ data);
+    if (error) {
+        console.log(error);
+        return res.status(500).json(error);
+    }
+
+    console.log(data);
+
+    res.json(data);
 });
 
 app.post('/products', async (req, res) => {
-    const {error} = await supabase
+
+    const { error } = await supabase
         .from('products')
         .insert({
             name: req.body.name,
             description: req.body.description,
             price: req.body.price,
         })
+
     if (error) {
-        res.send(error);
+        console.log(error);
+        return res.status(500).json(error);
     }
-    res.send("created!!");
-    console.log("retorno "+ req.body.name);
-    console.log("retorno "+ req.body.description);
-    console.log("retorno "+ req.body.price);
+
+    console.log(req.body);
+
+    res.json({
+        message: "created!!"
+    });
 
 });
 
 app.put('/products/:id', async (req, res) => {
-    const {error} = await supabase
+
+    const { error } = await supabase
         .from('products')
         .update({
             name: req.body.name,
@@ -82,22 +105,35 @@ app.put('/products/:id', async (req, res) => {
             price: req.body.price
         })
         .eq('id', req.params.id)
+
     if (error) {
-        res.send(error);
+        console.log(error);
+        return res.status(500).json(error);
     }
-    res.send("updated!!");
+
+    res.json({
+        message: "updated!!"
+    });
 });
 
 app.delete('/products/:id', async (req, res) => {
+
     console.log("delete: " + req.params.id);
-    const {error} = await supabase
+
+    const { error } = await supabase
         .from('products')
         .delete()
         .eq('id', req.params.id)
+
     if (error) {
-        res.send(error);
+        console.log(error);
+        return res.status(500).json(error);
     }
-    res.send("deleted!!")
+
+    res.json({
+        message: "deleted!!"
+    });
+
     console.log("delete: " + req.params.id);
 
 });
@@ -107,7 +143,7 @@ app.get('/', (req, res) => {
 });
 
 app.use((req, res) => {
-    res.send("Hello again I am working my friend to the moon and behind <3");
+    res.status(404).send("Hello again I am working my friend to the moon and behind <3");
 });
 
 app.listen(3000, '0.0.0.0', () => {
